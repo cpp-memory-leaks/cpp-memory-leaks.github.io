61f1e0829981590271c6f6dba44858b0b290417a
cpp-memory-leaks <1300692990@qq.com>
2024-12-10 22:30:37 +0800

    Site updated: 2024-12-10 22:30:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/06/Bug/","2024/12/05/hello-world/","2024/12/06/bugRecord/","2024/12/06/hhh/"];function toRandomPost(){
+var posts=["2024/12/05/hello-world/","2024/12/06/bugRecord/","2024/12/06/hhh/","2024/12/06/Bug/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
