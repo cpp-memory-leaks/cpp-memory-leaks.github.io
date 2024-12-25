1adbdc7f782f5557d6f46c976b34097f88fe723e
cpp-memory-leaks <1300692990@qq.com>
2024-12-25 23:52:30 +0800

    Site updated: 2024-12-25 23:52:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/06/Bug/","2024/12/25/QCheckBox的三态复选框和反选/"];function toRandomPost(){
+var posts=["2024/12/25/QCheckBox的三态复选框和反选/","2024/12/25/Qt使用libheif库显示苹果的heic图片/","2024/12/25/摇杆/","2024/12/25/Qt高性能列表控件之QListView的使用及优点/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
