860a55ad47c8df3833c201cfff38c68389180ba4
cpp-memory-leaks <1300692990@qq.com>
2024-12-25 20:40:15 +0800

    Site updated: 2024-12-25 20:40:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/06/Bug/","2024/12/06/bugRecord/","2024/12/05/hello-world/","2024/12/06/hhh/","2024/12/25/README/","2024/12/25/QCheckBox的三态复选框和反选/"];function toRandomPost(){
+var posts=["2024/12/06/Bug/","2024/12/06/bugRecord/","2024/12/05/hello-world/","2024/12/06/hhh/","2024/12/25/README/","2024/12/25/QCheckBox的三态复选框和反选/","2024/12/25/demo/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
