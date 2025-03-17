cdd4c30402ded5d5bf2e066e3d37f2655766ccb6
cpp-memory-leaks <1300692990@qq.com>
2025-03-17 20:13:08 +0800

    Site updated: 2025-03-17 20:12:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/25/QCheckBox的三态复选框和反选/","2025/01/06/Qt高性能列表控件之QListView的使用及优点/","2025/01/06/摇杆/","2025/01/06/使用Qt私有接口绘制图片作窗口阴影/","2025/01/06/Qt使用libheif库显示苹果的heic图片/","2025/01/06/使用libheif库读取heic文件的缩略图/","2025/01/17/CodeStyle/","2025/03/17/CodeStyle-1/"];function toRandomPost(){
+var posts=["2024/12/25/QCheckBox的三态复选框和反选/","2025/01/06/Qt高性能列表控件之QListView的使用及优点/","2025/01/06/摇杆/","2025/01/06/使用Qt私有接口绘制图片作窗口阴影/","2025/01/06/Qt使用libheif库显示苹果的heic图片/","2025/01/06/使用libheif库读取heic文件的缩略图/","2024/08/07/CodeStyle/","2025/03/17/CodeStyle-1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
